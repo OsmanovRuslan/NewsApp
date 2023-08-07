abc96982acef1eed696a7730b5182b45727dbca7
Ruslan <114252945+OsmanovRuslan@users.noreply.github.com>
2023-08-07 20:20:01 +0300

    Добавлена возможность смены тем

--- app/src/main/java/com/training/newsapp/MainActivity.kt
+++ app/src/main/java/com/training/newsapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.training.newsapp
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -10,14 +13,31 @@ import com.training.newsapp.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var prefs: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        prefs = getSharedPreferences("MODE", MODE_PRIVATE)
+        themeSetter(prefs.getString("theme", "1").toString())
+
+
         val bottomNavigationView = binding.bottomNavigationView
         val navController = binding.fragmentContainerView.getFragment<NavHostFragment>().navController
         bottomNavigationView.setupWithNavController(navController)
+    }
+    private fun themeSetter(themeCode: String) {
+        val themes = mapOf(
+            "1" to Pair(AppCompatDelegate.MODE_NIGHT_NO, "1"),
+            "2" to Pair(AppCompatDelegate.MODE_NIGHT_YES, "2"),
+            "3" to Pair(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM, "3")
+        )
+
+        themes[themeCode]?.let { (mode, modeTheme) ->
+            AppCompatDelegate.setDefaultNightMode(mode)
+            prefs.edit().putString("theme", modeTheme).apply()
+        }
     }
 }
--- app/src/main/java/com/training/newsapp/fragments/SettingsFragment.kt
+++ app/src/main/java/com/training/newsapp/fragments/SettingsFragment.kt
@@ -1,13 +1,22 @@
 package com.training.newsapp.fragments
 
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.training.newsapp.R
+import com.training.newsapp.databinding.BottomSheetDialogBinding
 import com.training.newsapp.databinding.FragmentSettingsBinding
 
 class SettingsFragment : ViewBindingFragment<FragmentSettingsBinding>() {
+
+    private val bottomSheetBinding by lazy { BottomSheetDialogBinding.inflate(layoutInflater) }
+    private lateinit var prefs: SharedPreferences
 
     override fun makeBinding(
         inflater: LayoutInflater,
@@ -19,7 +28,45 @@ class SettingsFragment : ViewBindingFragment<FragmentSettingsBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        prefs = requireContext().getSharedPreferences("MODE", MODE_PRIVATE)
+        when (prefs.getString("theme", "1")) {
+            "1" -> binding.textTheme.text = requireContext().getString(R.string.light)
+            "2" -> binding.textTheme.text = requireContext().getString(R.string.night)
+            "3" -> binding.textTheme.text = requireContext().getString(R.string.system)
+            else ->  binding.textTheme.text = requireContext().getString(R.string.light)
+        }
+
+        val bottomSheetDialog = BottomSheetDialog(requireContext())
+        bottomSheetDialog.setContentView(bottomSheetBinding.root)
+
+        binding.textTheme.setOnClickListener {
+            bottomSheetDialog.show()
+        }
+        bottomSheetBinding.btnLightTheme.setOnClickListener {
+            themeSetListener("1")
+            bottomSheetDialog.dismiss()
+        }
+        bottomSheetBinding.btnDarkTheme.setOnClickListener {
+            themeSetListener("2")
+            bottomSheetDialog.dismiss()
+        }
+        bottomSheetBinding.btnSystemTheme.setOnClickListener {
+            themeSetListener("3")
+            bottomSheetDialog.dismiss()
+        }
     }
 
+    private fun themeSetListener(themeCode: String) {
+        val themes = mapOf(
+            "1" to Triple(AppCompatDelegate.MODE_NIGHT_NO, R.string.light, "1"),
+            "2" to Triple(AppCompatDelegate.MODE_NIGHT_YES, R.string.night, "2"),
+            "3" to Triple(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM, R.string.system, "3")
+        )
 
+        themes[themeCode]?.let { (mode, stringRes, modeTheme) ->
+            AppCompatDelegate.setDefaultNightMode(mode)
+            binding.textTheme.text = requireContext().getString(stringRes)
+            prefs.edit().putString("theme", modeTheme).apply()
+        }
+    }
 }
